da897292562d38fce60646d41e310c5e5bb34ae1
nettk132 <nettk1322@gmail.com>
2024-02-22 00:07:57 +0700

    first commit

---
+++ resources/js/Pages/product/index.vue
@@ -0,0 +1,31 @@
+<script setup>
+import AppLayout from '@/Layouts/AppLayout.vue';
+import Welcome from '@/Components/Welcome.vue';
+import { ref } from 'vue';
+
+defineProps(['products']);
+
+</script>
+
+<template>
+    <AppLayout title="Dashboard">
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                product
+            </h2>
+        </template>
+
+        <div v-for="product in products" :key="product.id">
+  <h2>{{ product.name_product }}  {{ product.price }} บาท</h2>
+
+  </div>
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+
+                </div>
+            </div>
+        </div>
+    </AppLayout>
+</template>
